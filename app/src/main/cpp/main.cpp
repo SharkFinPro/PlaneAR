d7f82ab7203cb76b09cd448dc2d8ff462ef350da
Alex Martin <sharkfinpro@hotmail.com>
2025-10-28 21:52:17 -0700

    "Hello World" for Vulkan!

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -1,12 +1,41 @@
 #include <game-activity/native_app_glue/android_native_app_glue.h>
 #include <android/log.h>
 
+#include <vulkan/vulkan.h>
+#include <stdexcept>
+
 #define LOG_TAG "HelloWorld"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 
 void android_main(struct android_app *pApp)
 {
   LOGI("Hello World from C++!");
+
+  VkInstance instance;
+
+  VkApplicationInfo appInfo{};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "PlaneAR";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "No Engine";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
+
+  VkInstanceCreateInfo createInfo{};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+
+  createInfo.enabledExtensionCount = 0;
+
+  createInfo.enabledLayerCount = 0;
+
+  if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+    throw std::runtime_error("failed to create instance!");
+  }
+  else
+  {
+    LOGI("VK INSTANCE CREATED!");
+  }
 
   int events;
   struct android_poll_source* source;
@@ -29,4 +58,6 @@ void android_main(struct android_app *pApp)
       }
     }
   }
+
+  vkDestroyInstance(instance, nullptr);
 }
