f8729de301a7b59f62731e83c0cb76e55c5f06e9
Alex Martin <sharkfinpro@hotmail.com>
2026-01-12 11:55:11 -0800

    Add PipelineManager class skeleton with Vulkan methods
    Introduced the PipelineManager class with constructor, destructor, and method declarations for binding graphics pipelines, pushing pipeline constants, and binding descriptor sets. These changes lay the groundwork for managing Vulkan pipelines in the graphics engine.

--- app/src/main/cpp/GraphicsEngine/components/pipelines/PipelineManager.cpp
+++ app/src/main/cpp/GraphicsEngine/components/pipelines/PipelineManager.cpp
@@ -1,4 +1,39 @@
 #include "PipelineManager.h"
 
 namespace ge {
+  PipelineManager::PipelineManager(std::shared_ptr<LogicalDevice> logicalDevice,
+                                   const std::shared_ptr<Renderer>& renderer,
+                                   const std::shared_ptr<AssetManager>& assetManager)
+  {
+
+  }
+
+  PipelineManager::~PipelineManager()
+  {
+
+  }
+
+  void PipelineManager::bindGraphicsPipeline(const std::shared_ptr<CommandBuffer>& commandBuffer,
+                                             const PipelineType pipelineType) const
+  {
+
+  }
+
+  void PipelineManager::pushGraphicsPipelineConstants(const std::shared_ptr<CommandBuffer>& commandBuffer,
+                                                      const PipelineType pipelineType,
+                                                      const VkShaderStageFlags stageFlags,
+                                                      const uint32_t offset,
+                                                      const uint32_t size,
+                                                      const void* values) const
+  {
+
+  }
+
+  void PipelineManager::bindGraphicsPipelineDescriptorSet(const std::shared_ptr<CommandBuffer>& commandBuffer,
+                                                          const PipelineType pipelineType,
+                                                          VkDescriptorSet descriptorSet,
+                                                          const uint32_t location) const
+  {
+
+  }
 } // ge
--- app/src/main/cpp/GraphicsEngine/components/pipelines/PipelineManager.h
+++ app/src/main/cpp/GraphicsEngine/components/pipelines/PipelineManager.h
@@ -1,11 +1,41 @@
 #ifndef PLANEAR_PIPELINEMANAGER_H
 #define PLANEAR_PIPELINEMANAGER_H
 
+#include <vulkan/vulkan.h>
+#include <memory>
+
 namespace ge {
+
+  class AssetManager;
+  class CommandBuffer;
+  class LogicalDevice;
+  class Renderer;
+
+  enum class PipelineType;
 
   class PipelineManager
   {
+  public:
+    PipelineManager(std::shared_ptr<LogicalDevice> logicalDevice,
+                    const std::shared_ptr<Renderer>& renderer,
+                    const std::shared_ptr<AssetManager>& assetManager);
 
+    ~PipelineManager();
+
+    void bindGraphicsPipeline(const std::shared_ptr<CommandBuffer>& commandBuffer,
+                              PipelineType pipelineType) const;
+
+    void pushGraphicsPipelineConstants(const std::shared_ptr<CommandBuffer>& commandBuffer,
+                                       PipelineType pipelineType,
+                                       VkShaderStageFlags stageFlags,
+                                       uint32_t offset,
+                                       uint32_t size,
+                                       const void* values) const;
+
+    void bindGraphicsPipelineDescriptorSet(const std::shared_ptr<CommandBuffer>& commandBuffer,
+                                           PipelineType pipelineType,
+                                           VkDescriptorSet descriptorSet,
+                                           uint32_t location) const;
   };
 
 } // ge
