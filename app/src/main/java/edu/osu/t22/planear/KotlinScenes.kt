c7890d545d289e5a75b85f0f81e88cc396a1e329
Alex Martin <sharkfinpro@hotmail.com>
2026-02-13 15:14:07 -0800

    Add 2D renderer wrapper and sample scene
    Introduce Renderer2D JNI bindings and a GraphicsEngineWrapper to obtain the native renderer pointer (app/src/main/java/edu/osu/t22/planear/GraphicsEngine.kt). Add Scene1 (app/src/main/java/edu/osu/t22/planear/KotlinScenes.kt) which implements Scene.render to draw a light-blue background, title and subtitle text, and a tap/hover ellipse effect using the new renderer APIs. Native functions (external) are declared and expected to be provided by the native engine.

---
+++ app/src/main/java/edu/osu/t22/planear/KotlinScenes.kt
@@ -0,0 +1,27 @@
+package edu.osu.t22.planear
+
+class Scene1 : Scene {
+    override fun render(sceneInfo: SceneInfo) {
+        val engine = GraphicsEngineWrapper(sceneInfo.enginePtr)
+        val r = engine.getRenderer2D()
+
+        // Light blue background
+        r.fill(240, 248, 255)
+        r.rect(0f, 0f, sceneInfo.screenWidth, sceneInfo.screenHeight)
+
+        // Title
+        r.fill(0, 0, 0)
+        r.textFont("roboto", 100)
+        r.text("PlaneAR", 100f, 300f)
+
+        r.textSize(64)
+        r.text("An AR Plane Tracking App", 100f, 450f)
+
+        renderTapEffect(r, sceneInfo)
+    }
+
+    private fun renderTapEffect(r: Renderer2D, sceneInfo: SceneInfo) {
+        r.fill(255, 200, 0, 150)
+        r.ellipse(sceneInfo.mouseX - 15f, sceneInfo.mouseY - 15f, 30f, 30f)
+    }
+}
